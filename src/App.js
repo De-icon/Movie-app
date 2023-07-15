7815c42ac33db2eb2e811c8d081f85e353de4e8a
isaac john <isaacjohn2028@gmail.com>
2023-07-16 00:41:56 +0100

    fist commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      
+      <h1>Hi Mom</h1>
+      <h2>{2 + 9}</h2>
+      <img src={logo} alt='Logo' />
+    </div>
+  );
+}
+
+export default App;
